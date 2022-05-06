3298a2a4952c274d5b4697baea6ba77a18769fde
Ress <48500247+ress997@users.noreply.github.com>
2022-05-07 04:44:01 +0900

    タブにプロセス名を表示

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,18 @@
 local wezterm = require 'wezterm';
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = wezterm.truncate_right(utils.basename(tab.active_pane.foreground_process_name), max_width)
+	if title == "" then
+		title = wezterm.truncate_right(
+			utils.basename(utils.convert_home_dir(tab.active_pane.current_working_dir)),
+			max_width
+		)
+	end
+	return {
+		{ Text = tab.tab_index + 1 .. ":" .. title },
+	}
+end)
+
 return {
 	font = wezterm.font("HackGenNerd Console"),
 	use_ime = true,
